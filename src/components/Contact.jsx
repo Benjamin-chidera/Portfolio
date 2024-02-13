6ae3d52ef11bd76f162cafb15fafc9b2d25fa905
Benjamin chidera <benjaminchidera72@gmain.com>
2024-02-13 22:06:12 +0100

    refactor: linked my email address

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,24 +1,29 @@
-import React from 'react'
-import me from "../assets/me.png"
+import React from "react";
+import me from "../assets/me.png";
 import { TfiEmail } from "react-icons/tfi";
 import { FaExternalLinkAlt } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa6";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export const Contact = () => {
   return (
     <div className="flex flex-wrap gap-7 items-center mt-10 lg:mt-0 contact-2560">
       <img src={me} alt="" className="w-[66px] h-[66px] rounded-full " />
 
-      <Link className="flex items-center gap-2 " to="<EMAIL>">
+      <a
+        className="flex items-center gap-2 "
+        href="mailto:benjaminchidera72@gmail.com"
+        target="_blank"
+      >
         <TfiEmail color="#B5B5B5" />
         <p className="font-semibold text-[#B5B5B5]">Email Me</p>
         <FaExternalLinkAlt color="#B5B5B5" />
-      </Link>
+      </a>
 
       <Link
         className="flex items-center gap-2"
         to="https://github.com/Benjamin-chidera"
+        target="_blank"
       >
         <FaGithub color="#B5B5B5" />
         <p className="font-semibold text-[#B5B5B5]">GitHub</p>
@@ -26,4 +31,4 @@ export const Contact = () => {
       </Link>
     </div>
   );
-}
+};
